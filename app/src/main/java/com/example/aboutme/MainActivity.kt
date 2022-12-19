4368efba38864a52096fb6db6a8cebb47074e45d
eslaamehab <eslaam.ehaab@gmail.com>
2022-12-19 15:30:26 +0200

    Added editText & its button
    Replaced findViewById to view binding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.aboutme
 
+import android.content.Context
+import android.inputmethodservice.InputMethodService
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.doneButton.setOnClickListener(){
+            addNickname(it)
+        }
+
+//        binding.nicknameText.text = "test nickname "
+
     }
+
+    private fun addNickname(view: View){
+        val editText = binding.nicknameEdit
+        val nicknameText = binding.nicknameText
+
+        nicknameText.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+        // hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+
 }
